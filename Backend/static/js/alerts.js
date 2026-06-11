77a26d84b525bb1d4217ab93d17b2cb3d4cb18c8
Geethanjali <kondredigeethanjali18@gmail.comm>
2026-06-11 23:06:13 +0530

    dash board has been updated

--- Backend/static/js/alerts.js
+++ Backend/static/js/alerts.js
@@ -1,12 +1,8 @@
 async function loadAlerts() {
 
-    const response =
-        await fetch(
-            "/all-predictions"
-        );
+    const response = await fetch("/all-predictions");
 
-    const predictions =
-        await response.json();
+    const predictions = await response.json();
 
     let html = "";
 
@@ -14,12 +10,42 @@ async function loadAlerts() {
 
         if(item.prediction === "ANOMALY"){
 
+            let severity = "";
+            let severityClass = "";
+
+            if(item.confidence >= 90){
+                severity = "Critical";
+                severityClass = "severity-critical";
+            }
+            else if(item.confidence >= 70){
+                severity = "High";
+                severityClass = "severity-high";
+            }
+            else{
+                severity = "Medium";
+                severityClass = "severity-medium";
+            }
+
             html += `
             <tr>
                 <td>${item.server_name}</td>
-                <td>${item.prediction}</td>
+
+                <td>
+                    <span class="badge-danger">
+                        ${item.prediction}
+                    </span>
+                </td>
+
                 <td>${item.confidence}%</td>
+
+                <td>
+                    <span class="${severityClass}">
+                        ${severity}
+                    </span>
+                </td>
+
                 <td>${item.possible_causes.join(", ")}</td>
+
             </tr>
             `;
         }
